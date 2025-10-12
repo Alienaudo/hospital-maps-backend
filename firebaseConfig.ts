476664e0af88553918d0565b0b0c9e6e277c37cf
Alienaudo <contatotiago819@gmail.com>
2025-10-12 11:43:27 -0400

    Add: project id in firebase instance

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,7 +1,10 @@
 import { type App, cert, initializeApp } from 'firebase-admin/app';
 
+const projectId = process.env.GCLOUD_PROJECT || "hosptal-map";
+
 export const firebase: App = initializeApp({
 
+    projectId,
     credential: cert("./src/firebase/hosptal-map-firebase-adminsdk-fbsvc-c89ab7353b.json"),
 
 });
