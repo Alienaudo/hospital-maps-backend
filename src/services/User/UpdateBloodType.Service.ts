64123d1bd82747a005f6eba62a077cb2225ffd1a
alienaudo <contatotiago819@gmail.com>
2025-10-16 11:01:19 -0400

    Add: Add update bloodtype service

---
+++ src/services/User/UpdateBloodType.Service.ts
@@ -0,0 +1,93 @@
+import type { BloodType, PrismaClient } from "../../generated/client.js";
+import type { FastifyRequest } from "fastify/types/request.js";
+import type { FastifyReply } from "fastify/types/reply.js";
+import { StatusCodes } from "http-status-codes";
+import { logger } from "../../logger.js";
+
+export class UpdateBloodType {
+
+    private readonly prisma: PrismaClient;
+
+    constructor(prisma: PrismaClient) {
+
+        this.prisma = prisma;
+
+    };
+
+    public exec = async (
+
+        request: FastifyRequest<{
+            Body: {
+
+                id: string,
+                bloodType: BloodType
+
+            }
+        }>,
+        reply: FastifyReply
+
+    ): Promise<FastifyReply> => {
+
+        try {
+
+            const { id, bloodType } = request.body
+
+            const newBloodType = await this.prisma.user
+                .update({
+
+                    where: {
+
+                        id: id
+
+                    },
+                    data: {
+
+                        bloodType: bloodType
+
+                    },
+                    select: {
+
+                        bloodType: true,
+                        updatedAt: true
+
+                    },
+
+                });
+
+            return reply
+                .status(StatusCodes.OK)
+                .send({
+
+                    message: "Tipo sanguineo atualizado com êxito",
+                    booldType: newBloodType.bloodType,
+
+                })
+                .headers({
+
+                    "Content-Type": "application/json",
+                    "last-modified": `${newBloodType.updatedAt}`
+
+                });
+
+        } catch (error: unknown) {
+
+            logger.error(`Erro while updating blood type: ${JSON.stringify(error)}`);
+
+            return reply
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({
+
+                    message: "Ocorreu um erro ao processar a solicitação"
+
+                })
+                .headers({
+
+                    "Content-Type": "application/json",
+
+                });
+
+        };
+
+    };
+
+};
