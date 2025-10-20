01e58e89d7242e6cb4909a25bbeff965c27a22a4
alienaudo <contatotiago819@gmail.com>
2025-10-20 15:10:09 -0400

    Refactor: Refactor route and methods
    Adds Delete diseases methods
    Methods Get and
    
    Adds now receives user's uid from auth middleware

---
+++ src/services/Diseases/AddDiseases.Service.ts
@@ -0,0 +1,103 @@
+import type { PrismaClient } from "../../generated/client.js";
+import type { FastifyRequest } from "fastify/types/request.js";
+import type { FastifyReply } from "fastify/types/reply.js";
+import { StatusCodes } from "http-status-codes";
+import { logger } from "../../logger.js";
+import { PrismaClientKnownRequestError } from "../../generated/internal/prismaNamespace.js";
+
+export class AddDiseasesService {
+
+    private readonly prisma: PrismaClient;
+
+    constructor(prisma: PrismaClient) {
+
+        this.prisma = prisma;
+
+    };
+
+    public exec = async (
+
+        request: FastifyRequest<{
+
+            Body: {
+
+                disease: {
+
+                    name: string
+                    isChronic: boolean
+
+                }[];
+
+            },
+
+        }>,
+        reply: FastifyReply
+
+    ): Promise<FastifyReply> => {
+
+        try {
+
+            const uid: string | undefined = request.userFirebase?.uid;
+            const { disease } = request.body;
+
+            if (!uid) throw Error("Auth Middleware not privided user's uid");
+
+            const updated = await this.prisma.user
+                .update({
+
+                    where: { firebaseId: uid },
+
+                    data: {
+
+                        disease: { create: disease },
+
+                    },
+
+                    select: { updatedAt: true },
+
+                });
+
+            return reply
+                .status(StatusCodes.OK)
+                .headers({
+
+                    "Content-Type": "application/json;",
+                    "last-modified": `${updated.updatedAt.toISOString()}`,
+
+                })
+                .send({
+
+                    message: "Item adicionado com êxito",
+
+                });
+
+        } catch (error: unknown) {
+
+            if (error instanceof PrismaClientKnownRequestError && error.code === "P2025") {
+
+                return reply
+                    .status(StatusCodes.NOT_FOUND)
+                    .send({
+
+                        error: "Usuário não encontrado",
+
+                    });
+
+            };
+
+            logger.error(`Erro while updating blood type: ${error}`);
+
+            return reply
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({
+
+                    error: "Ocorreu um erro ao processar a solicitação",
+
+                });
+
+        };
+
+    };
+
+};
+
