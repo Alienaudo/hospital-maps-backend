5b739d99fd08ea2f5323caf5742ffbb19ce6cc93
alienaudo <contatotiago819@gmail.com>
2025-10-21 10:16:01 -0400

    Feat: Rename files and implements methods
    Rename folder to Medication
    Rename files
    
    Implements Add, Get and Delete methods

---
+++ src/services/Medication/AddMedication.Service.ts
@@ -0,0 +1,100 @@
+import type { FastifyRequest } from "fastify/types/request.js";
+import type { PrismaClient } from "../../generated/client.js";
+import type { FastifyReply } from "fastify/types/reply.js";
+import { StatusCodes } from "http-status-codes";
+import { PrismaClientKnownRequestError } from "../../generated/internal/prismaNamespace.js";
+import { logger } from "../../logger.js";
+
+export class AddMedication {
+
+    private readonly prisma: PrismaClient;
+
+    constructor(prisma: PrismaClient) {
+
+        this.prisma = prisma;
+
+    };
+
+    public exec = async (
+
+        request: FastifyRequest<{
+
+            Body: {
+
+                medication: {
+
+                    name: string
+                    isContinuousUse: boolean
+
+                }[];
+
+            },
+
+        }>,
+        reply: FastifyReply
+
+    ): Promise<FastifyReply> => {
+
+        try {
+
+            const uid: string | undefined = request.userFirebase?.uid;
+            const { medication } = request.body;
+
+            if (!uid) throw Error("Auth Middleware not privided user's uid");
+
+            const result = await this.prisma.user
+                .update({
+
+                    where: { firebaseId: uid },
+                    data: {
+
+                        medication: { create: medication },
+
+                    },
+                    select: { updatedAt: true },
+
+                });
+
+            return reply
+                .status(StatusCodes.OK)
+                .headers({
+
+                    "Content-Type": "application/json;",
+                    "last-modified": `${result.updatedAt.toISOString()}`,
+
+                })
+                .send({
+
+                    message: "Item adicionado com êxito",
+
+                });
+
+        } catch (error: unknown) {
+
+            if (error instanceof PrismaClientKnownRequestError && error.code === "P2025") {
+
+                return reply
+                    .status(StatusCodes.NOT_FOUND)
+                    .send({
+
+                        error: "Usuário não encontrado",
+
+                    });
+
+            };
+
+            logger.error(`Erro while adding medication: ${error}`);
+
+            return reply
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({
+
+                    error: "Ocorreu um erro ao processar a solicitação",
+
+                });
+
+        };
+
+    };
+
+};
